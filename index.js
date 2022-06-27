0d1b9be6bf8d2db38c8a8397bff4671d8e777fc5
Gleb <59769228+pPitohu@users.noreply.github.com>
2022-06-27 16:54:02 +0000

    some fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const colors = require('colors')
 const prompt = require('prompt')
-const {exec} = require('child_process')
+const { exec } = require('child_process')
 
 // const configs = ['config', 'config_', 'config_2', 'config_3', 'config_4']
 const configs = ['config', 'config_']
@@ -15,17 +15,13 @@ prompt.start()
 prompt.message = ''
 prompt.delimiter = ' >>';
 
-async function rename(number) {
+function rename(number) {
     try {
-        let second = configs[1];
+        const second = number == 2 ? configs[0] : configs[1];
 
-        if (number == 2) {
-            second = configs[0]
-        }
-
-        await fs.renameSync(appdata + second, appdata + 'config__')
-        await fs.renameSync(appdata + configs[number - 1], appdata + 'config')
-        await fs.renameSync(appdata + 'config__', appdata + 'config_')
+        fs.renameSync(appdata + second, appdata + 'config__')
+        fs.renameSync(appdata + configs[number - 1], appdata + 'config')
+        fs.renameSync(appdata + 'config__', appdata + 'config_')
 
         return true;
     } catch (e) {
@@ -34,56 +30,53 @@ async function rename(number) {
     }
 }
 
-async function getAccounts() {
+function getAccounts() {
     let i = 0;
     for (const config of configs) {
         i++
         try {
-            const session = (await fs.readFileSync(appdata + config, 'utf8')).split('\n')
+            const session = fs.readFileSync(appdata + config, 'utf8').split('\n')
 
             for (const line of session) {
-                const values = line.split(':')
-                if (values[0] === 'username') {
-                    let username = values[1].trim()
-                    if (username === '') {
+                const [key, value] = line.split(':')
+                if (key === 'username') {
+                    let username = value.trim()
+                    if (!username)
                         username = 'Новый аккаунт'
-                    }
 
                     console.log(`${i}) ${username}`.blue)
                 }
             }
         } catch (e) {
-            await fs.openSync(appdata + config, 'w')
+            fs.openSync(appdata + config, 'w')
             console.log(`${i}) Новый аккаунт`.cyan)
         }
     }
 }
 
-async function start() {
+function start() {
     console.log(`  __   ___            ___ _                           
  \\ \\ / (_)_ __  ___ / __| |_  __ _ _ _  __ _ ___ _ _ 
   \\ V /| | '  \\/ -_) (__| ' \\/ _\` | ' \\/ _\` / -_) '_|
    \\_/ |_|_|_|_\\___|\\___|_||_\\__,_|_||_\\__, \\___|_|  
                                        |___/         \n`.green)
-    await getAccounts()
+    getAccounts()
     console.log('Если в списке нет одного из аккаунтов, просто введите 1 или 2'.bold)
 
-    const selected = (await prompt.get({
+    const selected = await prompt.get({
         properties: {
             account: {
                 description: colors.magenta("\nВведите номер аккаунта")
             }
         }
-    }))['account']
+    })['account']
 
     if (selected < 1 || selected > 2) {
         console.log('Можно иметь одновременно лишь два аккаунта. Нарушаем?'.red)
-        while (1) {
-        }
+        while (1) {}
     }
 
-    const renamed = await rename(selected)
-    if (renamed) {
+    if (rename(selected)) {
         try {
             exec(appdata + 'vimeworld.exe')
             console.log('Запуск успешен'.green)
@@ -91,9 +84,8 @@ async function start() {
             console.error(e)
             console.log('Запуск сломафся'.red)
         }
-    } else {
-        console.log('Переименование конфига сломалось. Может быть, VimeWorld уже запущен?'.red)
-    }
+    } else console.log('Переименование конфига сломалось. Может быть, VimeWorld уже запущен?'.red)
+
 }
 
 start()
